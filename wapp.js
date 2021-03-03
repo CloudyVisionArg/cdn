ae1de7085b4879dc32830c52104e34a5c14bb67e
Jorge Pagano <jorgepagano@gmail.com>
2021-03-03 16:44:02 -0300

    -

--- wapp.js
+++ wapp.js
@@ -146,7 +146,7 @@ var wapp = {
 	
 	renderChat: function (pCont) {
 		var $heading = $('<div/>', {
-			class: 'row wapp-heading',
+			class: 'wapp-row wapp-heading',
 		}).appendTo(pCont);
 		
 		var $headingLeft = $('<div/>', {
