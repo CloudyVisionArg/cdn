ee5f5e56caa4114d84cfd568dc4b6ce43c519d16
Jorge Pagano <jorgepagano@gmail.com>
2021-03-04 15:16:24 -0300

    -

--- wapp.js
+++ wapp.js
@@ -402,7 +402,7 @@ var wapp = {
 			
 			var $row = $('<div/>', {
 				class: 'wapp-message',
-				style: 'width: 100%;',
+				style: 'width: 100%; display: inline-block;',
 				'data-sid': pMsg.sid,
 				'data-date': pMsg.date,
 			});
