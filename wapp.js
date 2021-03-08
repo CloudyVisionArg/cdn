d12d680eef629c9e3c95c968c9e559de760013c0
Jorge Pagano <jorgepagano@gmail.com>
2021-03-08 14:12:48 -0300

    -

--- wapp.js
+++ wapp.js
@@ -704,7 +704,7 @@ var wapp = {
 											$cont.scrollTop($cont[0].scrollHeight);
 									}
 								}
-							}, 1000)
+							}, 2000)
 		                },
 		                function (err) {
 							console.log(err);
