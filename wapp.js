bdbba868cce7c0856aa4ce2ab129f5242a4e0b6c
Jorge Pagano <jorgepagano@gmail.com>
2021-03-08 10:42:00 -0300

    -

--- wapp.js
+++ wapp.js
@@ -636,6 +636,7 @@ var wapp = {
 		};
 
 		xhr.open('POST', url, true);
+		xhr.withCredentials = true;
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 		xhr.send(payload);
 
