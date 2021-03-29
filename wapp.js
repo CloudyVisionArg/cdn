e5d86d6f06f1a037aec4137ef497a3cac4679705
Jorge Pagano <jorgepagano@gmail.com>
2021-03-29 10:33:58 -0300

    -

--- wapp.js
+++ wapp.js
@@ -279,6 +279,24 @@ var wapp = {
 				class: 'wapp-footer',
 			}).appendTo($cont);
 
+
+			$reply.append(`
+			<div class="dropup">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropup
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+			`)
+
+			/*
 			// Boton + (media)
 			var $div = $('<div/>', {
 				class: 'wapp-button',
@@ -295,6 +313,7 @@ var wapp = {
 					class: 'f7-icons',
 				}).append('plus').appendTo($div);
 			}
+			*/
 
 			$media.click(function (e) {
 				if (typeof(cordova) != 'object') {
@@ -384,7 +403,6 @@ var wapp = {
 						]
 					});
 
-					debugger;
 					mediaActions.params.chatEl = $cont[0];
 					mediaActions.open();
 				}
