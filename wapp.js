bf578be88c514b55d921d8ebe61c90ade56a17f8
Jorge Pagano <jorgepagano@gmail.com>
2021-04-06 19:13:38 -0300

    -

--- wapp.js
+++ wapp.js
@@ -302,7 +302,7 @@ var wapp = {
 				<ul class="dropdown-menu">
 					<li><a href="#">Archivo</a></li>
 					<li class="dropdown-submenu">
-						<a class="plantillas" tabindex="-1" href="#">Plantilla</a>
+						<a class="plantillas" href="#">Plantilla</a>
 						<ul class="dropdown-menu">
 							<li><a href="#">Plantilla 1</a></li>
 							<li><a href="#">Plantilla 2</a></li>
