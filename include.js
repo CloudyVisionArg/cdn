bd8936017403ec0abf6dfd1115e9e8b0b8fd3036
Jorge Pagano <jorgepagano@gmail.com>
2022-03-31 11:12:25 -0300

    /

--- include.js
+++ include.js
@@ -58,8 +58,8 @@ function registeredScripts() {
 
 	scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 0 });
 	scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 0 });
-	scripts.push({ id: 'lib-numeral', path: '/lib/numeral/numeral.min.js', version: 58 });
-	scripts.push({ id: 'lib-numeral-locales', path: '/lib/numeral/locales.min.js', version: 58 });
+	scripts.push({ id: 'lib-numeral', path: '/lib/numeral/numeral.min.js', version: 59 });
+	scripts.push({ id: 'lib-numeral-locales', path: '/lib/numeral/locales.min.js', version: 59 });
 	scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 57 });
 	scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 56 });
 	scripts.push({ id: 'lib-cryptojs-aes', path: '/lib/crypto-js/aes.js', version: 55 });
