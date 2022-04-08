662814a595ea5f6f6f445adc6cf137f4a32add54
Jorge Pagano <jorgepagano@gmail.com>
2022-04-08 14:08:57 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -26,8 +26,8 @@ arrScripts.push({ id: 'web-javascript', version: 0 });
 arrScripts.push({ id: 'web-controls' });
 arrScripts.push({ id: 'lib-numeral' });
 arrScripts.push({ id: 'lib-numeral-locales', depends: ['lib-numeral'] });
-arrScripts.push({ id: 'tempus-dominus', src: 'https://cdn.jsdelivr.net/gh/Eonasdan/tempus-dominus@master/dist/js/tempus-dominus.js', depends: ['popper'] });
-arrScripts.push({ id: 'tempus-dominus-css', src: 'https://cdn.jsdelivr.net/gh/Eonasdan/tempus-dominus@master/dist/css/tempus-dominus.css' });
+arrScripts.push({ id: 'tempus-dominus', src: 'https://cdn.jsdelivr.net/gh/Eonasdan/tempus-dominus@v6-beta/dist/js/tempus-dominus.js', depends: ['popper'] });
+arrScripts.push({ id: 'tempus-dominus-css', src: 'https://cdn.jsdelivr.net/gh/Eonasdan/tempus-dominus@v6-beta/dist/css/tempus-dominus.css' });
 arrScripts.push({ id: 'lib-moment' });
 
 include(arrScripts, function () {
