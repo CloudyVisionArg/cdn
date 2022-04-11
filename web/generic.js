58ba02c4c9755f3941b618bc3afb800ef97092cd
Jorge Pagano <jorgepagano@gmail.com>
2022-04-11 18:33:54 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -21,7 +21,7 @@ arrScripts.push({ id: 'bootstrap', src: 'https://cdn.jsdelivr.net/npm/bootstrap@
 arrScripts.push({ id: 'bootstrap-css', src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' });
 arrScripts.push({ id: 'bootstrap-icons', src: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' });
 arrScripts.push({ id: 'font-awesome', src: 'https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css' });
-arrScripts.push({ id: 'app7-doorsapi', depends: ['jquery'] });
+arrScripts.push({ id: 'doorsapi', depends: ['jquery'] });
 arrScripts.push({ id: 'web-javascript', version: 0 });
 arrScripts.push({ id: 'web-controls' });
 arrScripts.push({ id: 'lib-numeral' });
