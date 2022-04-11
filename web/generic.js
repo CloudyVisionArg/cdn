5cbc8e4a4a5b3ba0677a094548363ecc66812c60
Jorge Pagano <jorgepagano@gmail.com>
2022-04-11 12:55:38 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -342,7 +342,7 @@ function renderPage() {
     fillControls();
 
     $cont.append(`
-    <div class="toast-container">
+    <div class="toast-container position-absolute p-3 top-0 end-0">
 
   <div id="t1" class="toast">
     <div class="toast-header">
