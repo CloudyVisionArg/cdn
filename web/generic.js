514b8a5bd0b58c35a2256c60c20c2879e78dda19
Jorge Pagano <jorgepagano@gmail.com>
2022-04-11 10:45:30 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -194,7 +194,7 @@ function renderPage() {
                 <span class="d-none d-md-inline-block"> Guardar y salir</span>
             </button>
             <button type="button" id="cancel" class="btn btn-primary" onclick="exitForm(false);">
-                <i class="bi bi-box-arrow-right"></i>
+                <i class="bi bi-caret-right-fill"></i>
                 <span class="d-none d-md-inline-block"> Salir</span>
             </button>
     </div>
