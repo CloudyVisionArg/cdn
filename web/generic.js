a28987b820ad69e35d24be3abdeb7752512baa00
Jorge Pagano <jorgepagano@gmail.com>
2022-04-13 09:33:05 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -32,7 +32,8 @@ arrScripts.push({ id: 'tempus-dominus-css', src: 'https://cdnjs.cloudflare.com/a
 arrScripts.push({ id: 'lib-moment' });
 arrScripts.push({ id: 'bootstrap-select', src: 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js' });
 arrScripts.push({ id: 'bootstrap-select-css', src: 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/css/bootstrap-select.min.css' });
-arrScripts.push({ id: 'bootstrap-select-translation', src: 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/i18n/defaults-es_ES.min.js' });
+// todo: esto deberia ser segun el lenguaje
+arrScripts.push({ id: 'bootstrap-select-lang', src: 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/i18n/defaults-es_ES.min.js' });
 
 include(arrScripts, function () {
     preloader.show();
