01525e00e691a391158271757b016103566365fe
Jorge Pagano <jorgepagano@gmail.com>
2022-04-14 15:20:20 -0300

    /

--- web/controls.js
+++ web/controls.js
@@ -406,7 +406,7 @@ function newCKEditor(pId, pLabel, pValue) {
             scayt_autoStartup: true,
             //scayt_srcUrl: 'https://svc.webspellchecker.net/spellcheck31/wscbundle/wscbundle.js',
 
-            readOnly: false,
+            readOnly: true,
 
             height: 200,
 
