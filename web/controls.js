b8a9038ad6797073f9cc2bf2871f7ecd5e322aaa
Jorge Pagano <jorgepagano@gmail.com>
2022-04-14 13:22:27 -0300

    /

--- web/controls.js
+++ web/controls.js
@@ -374,7 +374,7 @@ function newHtmlArea(pId, pLabel, pValue) {
     scriptLoaded('tiny-mce', function () {
         tinymce.init({
             selector: '#' + pId,
-            plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments ',
+            plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
             toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
             toolbar_mode: 'floating',
             tinycomments_mode: 'embedded',
