d0273170ca748e3593b020ebf46a771fa3128f15
Jorge Pagano <jorgepagano@gmail.com>
2022-04-19 12:24:41 -0300

    /

--- web/controls.js
+++ web/controls.js
@@ -399,7 +399,7 @@ function newMapsAutocomplete(pId, pLabel) {
 
     var $inp = newInputText(pId, pLabel);
 
-    addInputButton($inp.find('input'), 'bi bi-geo-alt_fill');
+    addInputButton($inp.find('input'), 'bi bi-geo-alt-fill');
 
     return $inp;
     /*
