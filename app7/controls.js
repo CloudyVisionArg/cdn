af98bbc748373598936c4ffda35d88326c282a55
Jorge Pagano <jorgepagano@gmail.com>
2022-04-19 17:55:44 -0300

    /

--- app7/controls.js
+++ app7/controls.js
@@ -223,6 +223,7 @@ function getInputAddress(pId, pLabel, pValue) {
         maps.pickLocation($input[0], e);
     });
 
+    debugger;
     include('maps', 0, function () {
         $input.attr('data-filling', '1');
         maps.initAc($input[0], function () {
