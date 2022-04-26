bafab979018b97d5ef7b861ee5624007908e9373
Jorge Pagano <jorgepagano@gmail.com>
2022-04-26 13:04:13 -0300

    /

--- web/controls.js
+++ web/controls.js
@@ -455,7 +455,7 @@ function newDocLog(pTitle, pDocId, pCallback) {
     }).appendTo($cardBody);
 
 	$thead = $('<thead/>').appendTo($table);
-    $thead.append('<tr><th>Campo</th><th>Valor nuevo (click en la fila para ver el anterior)</th></tr>');
+    $thead.append('<tr><th>Campo</th><th>Valor nuevo <i class="bi bi-info-circle-fill"></i></th></tr>');
 
 	$tbody = $('<tbody/>').appendTo($table);
 
