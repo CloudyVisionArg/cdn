0952f5b450133e25682314d2b401cd7854fdc773
Jorge Pagano <jorgepagano@gmail.com>
2022-05-18 12:49:39 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -105,7 +105,7 @@ include(arrScripts, function () {
 });
 
 function end(pErr) {
-    logAndToast(errMsg(pErr), { delay: 10000 });
+    logAndToast(errMsg(pErr), { delay: 0 });
     preloader.hide();
 }
 
