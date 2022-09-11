87bda2e87e4ce9df7e2853b3774d1f2419d8175a
Jorge Pagano <jorgepagano@gmail.com>
2022-09-11 09:30:54 -0300

    /

--- app7/explorer.js
+++ app7/explorer.js
@@ -215,7 +215,7 @@ DoorsAPI.foldersGetById(fld_id).then(
                 resolve({ component: function (props, context) {
                     context.$on('pageInit', pageInit);
                     debugger;
-                    return () => context.$h``;
+                    return () => context.$h`holaaa`;
                     //context.$el = $page[0];
                     //return $render;
                 } });
