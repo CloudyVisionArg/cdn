716a9cd837691f3c1676b10cf82dc6940a2f9d3b
Jorge Pagano <jorgepagano@gmail.com>
2022-09-26 13:29:06 -0300

    /

--- include.js
+++ include.js
@@ -68,8 +68,8 @@ function registeredScripts() {
     Incluye la dependencia y setea el _hasdep del nodo a false
     */
 
+	scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 116, hasdep: true });
     scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 115 });
-	scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 115, hasdep: true });
 	scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 113 });
 	scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 112 });
 	scripts.push({ id: 'app7-scrversions', path: '/app7/scrversions.js', version: 111 });
