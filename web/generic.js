cb7e29cdf302fa327b6fb7bb671a6893cfe42c60
Jorge Pagano <jorgepagano@gmail.com>
2022-09-29 09:26:59 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -478,7 +478,7 @@ function renderPage() {
         title: 'Enviar a la papelera',
     });
 
-    $delBtn.append('<i class="bi bi-trash" aria-hidden="true"></i>');
+    //$delBtn.append('<i class="bi bi-trash" aria-hidden="true"></i>');
     $delBtn.appendTo($cont);
 
     /*
