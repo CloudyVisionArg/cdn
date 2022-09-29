f723e0810c938fbe672b55676e5ca0ded7081b8f
Jorge Pagano <jorgepagano@gmail.com>
2022-09-29 15:49:41 -0300

    /

--- include.js
+++ include.js
@@ -68,15 +68,16 @@ function registeredScripts() {
     Incluye la dependencia y setea el _hasdep del nodo a false
     */
 
-    scripts.push({ id: 'jslib', path: '/jslib.js', version: 124 });
-	scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 123 });
-	scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 122, hasdep: true });
-    scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 120 });
-	scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 117 });
-	scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 112 });
+    scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 125 });
+	scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 125 });
+	scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 125, hasdep: true });
+	scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 125 });
+    scripts.push({ id: 'jslib', path: '/jslib.js', version: 125 });
+	scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 125 });
+    scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 125 });
+	scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 125, hasdep: true });
+    scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 112 });
 	scripts.push({ id: 'app7-scrversions', path: '/app7/scrversions.js', version: 111 });
-	scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 111 });
-	scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 108 });
 	scripts.push({ id: 'app7-dsession', path: '/app7/dsession.js', version: 108 });
 	scripts.push({ id: 'app7-chpass', path: '/app7/chpass.html', version: 108 });
 	scripts.push({ id: 'app7-login', path: '/app7/login.html', version: 108 });
@@ -86,7 +87,6 @@ function registeredScripts() {
 	scripts.push({ id: 'lib-filesaver', path: '/lib/FileSaver.js', version: 98 });
 	scripts.push({ id: 'maps', path: '/maps.js', version: 96 });
 	scripts.push({ id: 'lib-fireworks', path: '/lib/fireworks.js', version: 93 });
-	scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 92, hasdep: true });
 	scripts.push({ id: 'emojis', path: '/emojis.js', version: 91 });
 	scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 84 });
 	scripts.push({ id: 'app7-console', path: '/app7/console.html', version: 73 });
