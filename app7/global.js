fd73633957f39abcd12843fbd0f51cc2961582d6
Jorge Pagano <jorgepagano@gmail.com>
2022-10-19 11:53:53 -0300

    /

--- app7/global.js
+++ app7/global.js
@@ -86,7 +86,7 @@ resolveRoute({
 function resolveRoute(pArgs) {
     var options = {};
 
-    if (f7ver >= 6) {
+    if (f7version >= 6) {
         options.on = {};
         if (typeof pArgs.pageInit == 'function') {
             options.on.pageInit = pArgs.pageInit;
