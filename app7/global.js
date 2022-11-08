f63a1c4c50fd8cc42b91c3c78b83e219f199a71c
Jorge Pagano <jorgepagano@gmail.com>
2022-11-08 09:46:17 -0300

    /

--- app7/global.js
+++ app7/global.js
@@ -1336,15 +1336,12 @@ function f7AppEvents() {
     })
 
     app7.on('pageBeforeIn', function (el) {
-        debugger;
         if (app7.theme == 'ios' && el.$pageEl.attr('id').substring(0, 9) == 'explorer_') {
-            if (el.$navbarEl.hasClass('with-searchbar-expandable-enabled')) {
-                var searchbar = app7.searchbar.get(el.$navbarEl.find('.searchbar'));
-                debugger;
-                if (searchbar.enabled) {
-                    el.$navbarEl.removeClass('with-searchbar-expandable-enabled');
-                    searchbar.$el.hide();
-                }
+            var searchbar = app7.searchbar.get(el.$navbarEl.find('.searchbar'));
+            debugger;
+            if (searchbar.enabled) {
+                el.$navbarEl.addClass('with-searchbar-expandable-enabled');
+                searchbar.$el.show();
             }
         }
     })
