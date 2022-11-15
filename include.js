a32f6a807bfe36ac35d3a382413d5fdf2717d77a
Jorge Pagano <jorgepagano@gmail.com>
2022-11-15 19:12:54 -0300

    /

--- include.js
+++ include.js
@@ -67,15 +67,14 @@ function registeredScripts() {
 
     Incluye la dependencia y setea el _hasdep del nodo a false
     */
+    scripts.push({ id: 'app7-cloudy-index.css', path: '/app7/cloudy/index.css', version: 150 });
 	scripts.push({ id: 'app7-login', path: '/app7/login.html', version: 149 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 149, hasdep: true });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 149 });
     scripts.push({ id: 'app7-index.css', path: '/app7/index.css', version: 149 });
-    scripts.push({ id: 'app7-cloudy-index.css', path: '/app7/cloudy/index.css', version: 149 });
     scripts.push({ id: 'app7-cloudy-index', path: '/app7/cloudy/index.js', version: 149 });
     scripts.push({ id: 'app7-sade-index.css', path: '/app7/sade/index.css', version: 149 });
     scripts.push({ id: 'app7-sade-index', path: '/app7/sade/index.js', version: 149 });
-
     scripts.push({ id: 'app7-notifications', path: '/app7/notifications.js', version: 147 });
     scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 146 });
 	scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 146 });
