eaa411684b1d26f356071739cca754ce856fc5e5
Jorge Pagano <jorgepagano@gmail.com>
2023-01-18 14:51:44 -0300

    /

--- app7/index.js
+++ app7/index.js
@@ -179,7 +179,7 @@ var app = {
         function loadJS(url, routeTo, routeFrom, resolve, reject) {
             $.ajax({
                 url: url,
-                dataType: 'text/plain',
+                dataType: 'text',
             }).done(function (data, textStatus, jqXHR) {
                 if (device.platform == 'browser') {
                     eval(data);
