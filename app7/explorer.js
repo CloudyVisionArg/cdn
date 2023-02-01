4293604ead48e77398bd5332e08eb36cd1318c7d
Jorge Pagano <jorgepagano@gmail.com>
2023-02-01 18:55:02 -0300

    /

--- app7/explorer.js
+++ app7/explorer.js
@@ -21,7 +21,7 @@ var propViewsFilter = 'App7_viewsFilter';
 var propInit = 'App7_explorerInit';
 var maxLen = 200;
 
-DoorsAPI.foldersGetById(fld_id).then(
+dSession.foldersGetById(fld_id).then(
     function (fld) {
         folder = fld;
         getFolderElements(folder);
