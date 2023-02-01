a312cc95227d27790513f03f80c950a3e84a39c0
Jorge Pagano <jorgepagano@gmail.com>
2023-02-01 11:13:46 -0300

    /

--- app7/controls.js
+++ app7/controls.js
@@ -1808,8 +1808,7 @@ function addDefaultOptions(pContainer) {
         title: 'Version de los scripts',
         mdicon: 'code',
         click: ev => {
-            debugger;
-            var view = app7.views.get($(this).closest('.view')[0]);
+            var view = app7.views.get($(ev.target).closest('.view')[0]);
             view.router.navigate('/cdn/?script=app7-scrversions');
         },
     }).appendTo(pContainer);
