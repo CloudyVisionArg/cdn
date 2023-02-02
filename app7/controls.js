0ad7217f441e5820a6b7547b7d4b6e5fef637bed
Jorge Pagano <jorgepagano@gmail.com>
2023-02-02 09:11:32 -0300

    /

--- app7/controls.js
+++ app7/controls.js
@@ -1421,7 +1421,7 @@ function getPage(params) {
             return getLink({ mdicon: '<span class="material-icons-outlined">cloud_upload</span>' }); //cloud_done
         else if (pButton == 'exit')
             //return getLink({ iosicon: 'chevron_left', mdicon: 'arrow_back_ios' });
-            return getLink({ mdicon: 'arrow_back_ios' });
+            return getLink({ mdicon: 'arrow_back_ios_new' });
         else if (pButton == 'search')
             return getLink({ iosicon: 'search', mdicon: 'search' });
         else
