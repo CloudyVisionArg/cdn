da5c95b3bda6a7f7e34bae0604553d839db4162d
Jorge Pagano <jorgepagano@gmail.com>
2023-02-07 11:44:13 -0300

    /

--- app7/import.js
+++ app7/import.js
@@ -48,16 +48,49 @@ $inputFile.change(async e => {
     sheet._rangeRows();
     sheet._rangeCells(0, 0);
 
+    $ul.empty();
+
     var headers = [];
     for (var i = 0; i < sheet._rangeCols(); i++) {
         headers.push(sheet._rangeCells(0, i).v);
+
+        getSelect('c' + i, headers[i]).appendTo($ul);
     }
+
+
+    
     debugger;
 
-
+    for 
 
 });
 
+$pageCont.append('<div class="block-title">Mapeo de campos</div>');
+
+var $list = $('<div/>', {
+    class: 'list no-hairlines-md',
+    style: 'margin-top: 0;',
+}).appendTo($pageCont);
+
+var $ul = $('<ul/>').appendTo($list);
+
+
+<ul><li class="item-content item-input item-input-with-value"><div class="item-media"><i class="f7-icons ios-only">paperclip</i></div><div class="item-inner"><div class="item-title item-floating-label">Excel</div><div class="item-input-wrap"><input type="text" id="ee" placeholder="Excel" autocomplete="off"><span class="input-clear-button"></span></div></div><input type="file" style="display: none" class="input-with-value"></li></ul></div>
+
+
+
+<div class="block-title">Full Layout / Inline Labels</div>
+<div class="list inline-labels no-hairlines-md">
+  <ul>
+    <li class="item-content item-input">
+      <div class="item-inner">
+        <div class="item-title item-label">Name</div>
+        <div class="item-input-wrap">
+          <input type="text" placeholder="Your name" />
+          <span class="input-clear-button"></span>
+        </div>
+      </div>
+    </li>
 
 
 
