fe12e5fbcf9babd5b37a11f9f906b431af36b0d8
Jorge Pagano <jorgepagano@gmail.com>
2023-02-09 16:46:49 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -16,7 +16,7 @@ CKEditor: https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR.html
 
 'use strict';
 
-var doorsapi2; urlParams, fld_id, folder, doc_id, doc;
+var doorsapi2, urlParams, fld_id, folder, doc_id, doc;
 var controlsFolder, controls, controlsRights;
 var saving, cache, lsScripts;
 
