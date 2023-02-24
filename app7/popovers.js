4916ccf8719ef37875af2da1fd228aa1ce2cc1a6
Daniel Aguilera <aguilera@cloudyvision.com.ar>
2023-02-24 16:28:17 -0300

    dbg

--- app7/popovers.js
+++ app7/popovers.js
@@ -77,13 +77,7 @@ function crearCarteles(pCartel,index,array){
     elTextCartel.innerHTML = pCartel["text"];
     divBlock.append(elTextCartel);
 
-    const elButton = document.createElement("button");
-    elButton.classList.add("button"); 
-    elButton.setAttribute("type","button");
-    elButton.innerText = "Ok"
-    elButton.onclick=function(){console.log(this)};
-    divBlock.append(elButton);
-
+    debugger;
 
     const text = div.outerHTML;
     const dynamicPopover = app7.popover.create({
