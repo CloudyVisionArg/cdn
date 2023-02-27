648ffc3b814c8306de48bbb57003a4a873f544ee
santiagopagnone <spagnone@gmail.com>
2023-02-27 10:24:06 -0300

    Attach correcion de optencion archivos de cache para descarga

--- app7/generic.js
+++ app7/generic.js
@@ -1373,9 +1373,7 @@ function downloadAtt(e) {
 }
 
 function openAtt(pURL) {
-    debugger;
     if (pURL.substring(0, 10) == 'cdvfile://' || pURL.includes("__cdvfile_cache__")) {
-        
         window.resolveLocalFileSystemURL(pURL,
             function (fileEntry) {
                 openFile(fileEntry.nativeURL);
