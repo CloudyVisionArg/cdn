a589462dc6bc05d4f33752e249f06b35d4dc8039
Jorge Pagano <jorgepagano@gmail.com>
2023-02-28 16:52:12 -0300

    /

--- include.js
+++ include.js
@@ -75,7 +75,7 @@ function registeredScripts() {
     Incluye la dependencia y setea el _hasdep del nodo a false
     */
 
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 222 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 223 });
     scripts.push({ id: 'jslib', path: '/jslib.js', version: 222 });
 
     scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 220 });
