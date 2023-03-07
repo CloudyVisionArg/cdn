567f711cb8d787cdbf19d737b7bc686a53501680
Jorge Pagano <jorgepagano@gmail.com>
2023-03-07 00:38:49 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -972,6 +972,7 @@ class Database {
         var res = await this.session.utils.execVbs(`
             Set rcs = dSession.Db.OpenRecordset("${ sql.replaceAll('"', '""') }")
             rcs.Save Response, 1
+            rcs.Close
             Response.End
         `);
     
