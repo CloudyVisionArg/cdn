b564f76727bb5d31b75be70e310206f72803dfc8
Jorge Pagano <jorgepagano@gmail.com>
2023-03-07 00:45:16 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -990,8 +990,6 @@ class Database {
         });
         var json = parser.parse(txt);
 
-        debugger;
-        
         var ret = [];
         var rows = json.xml.data.row;
         if (rows) {
