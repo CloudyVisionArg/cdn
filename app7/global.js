20b03b9e9079ab842866c2adb766315ccb7141e8
santiagopagnone <spagnone@gmail.com>
2023-03-08 14:49:28 -0300

    .

--- app7/global.js
+++ app7/global.js
@@ -2045,3 +2045,21 @@ function audioRecorder(pCallback) {
     }
 }
 
+//Donde va esto
+//Plugin Camera
+const CameraResultType = {
+    Uri: 'uri',
+    Base64: 'base64',
+    DataUrl: 'dataUrl'
+};
+
+const CameraSource = {
+    Prompt: 'PROMPT', //Prompts the user to select either the photo album or take a photo.
+    Camera: 'CAMERA', //Take a new photo using the camera.
+    Photos: 'PHOTOS' //Pick an existing photo from the gallery or photo album.
+};
+
+const CameraDirection = {
+    Rear: 'REAR',
+    Front: 'FRONT'
+};
