89872c64c4eb9cfbf05518f2bfcf4c6e6d7e1f6f
Jorge Pagano <jorgepagano@gmail.com>
2023-03-08 12:32:51 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -2217,13 +2217,9 @@ class Push {
         if (msg.data) {
             if (!msg.data.guid) msg.data.guid = this.session.utils.getGuid();
             notW.JsonExtraParameters = JSON.stringify(msg.data, (key, value) => {
-                if (typeof(value) == 'number' || typeof(value) == 'boolean') {
-                    return value.toString();
-                } else if (value == null) {
-                    return 'null';
-                } else {
-                    return value;
-                }
+                if (key == '') return value;
+                if (typeof(value) == 'number' || typeof(value) == 'boolean') return value.toString();
+                debugger;
             });
         }
 
