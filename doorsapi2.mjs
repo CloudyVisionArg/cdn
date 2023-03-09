5db51dd212b10dedf796133f23b9cf23daa105a5
Jorge Pagano <jorgepagano@gmail.com>
2023-03-09 11:17:03 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -2632,6 +2632,7 @@ class Utilities {
     
         if (!res.ok) {
             var txt = await res.text();
+            var js = JSON.parse(txt);
             var err = new Error();
             err.name = js.source;
             err.message = js.description + ' at line ' + js.line + '\n' + js.code;
