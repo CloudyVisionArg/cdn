3cb7de2b2b062d8f2dde779857d962e4883c8711
Jorge Pagano <jorgepagano@gmail.com>
2023-03-11 16:47:35 -0300

    /

--- app7/generic.js
+++ app7/generic.js
@@ -843,7 +843,7 @@ async function renderControls(pCont, pParent) {
 
         try {
             if (ctl['APP7_SCRIPT']) {
-                var ec = new evalCode();
+                var ec = evalCode.bind();
                 await ec([ctl['APP7_SCRIPT']]);
             }
         } catch (err) {
