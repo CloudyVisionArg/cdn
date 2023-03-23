87322cc9280ee9d7ba19da20e4646575bc3782b3
Jorge Pagano <jorgepagano@gmail.com>
2023-03-23 15:55:39 -0300

    /

--- include.js
+++ include.js
@@ -66,6 +66,7 @@ function registeredScripts() {
     Incluye la dependencia y setea el _hasdep del nodo a false
     */
 
+    scripts.push({ id: 'jslib', path: '/jslib.js', version: 244 });
     scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 243 });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 240 });
     scripts.push({ id: 'app7-import', path: '/app7/import.js', version: 238 });
@@ -74,7 +75,6 @@ function registeredScripts() {
     scripts.push({ id: 'app7-popovers.json', path: '/app7/popovers.json', version: 229 });
     scripts.push({ id: 'app7-popovers', path: '/app7/popovers.js', version: 229 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 232, hasdep: true });
-    scripts.push({ id: 'jslib', path: '/jslib.js', version: 227 });
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 226 });
     scripts.push({ id: 'app7-session', path: '/app7/session.mjs', version: 225 });
     scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 224 });
