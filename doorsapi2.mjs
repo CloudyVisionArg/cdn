9db128b0605eaa5dcafcfabf3386ff312d09863f
Jorge Pagano <jorgepagano@gmail.com>
2023-03-31 12:53:35 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -3898,12 +3898,12 @@ class RestClient {
             // Opciones por defecto estan marcadas con un *
             fetch(completeUrl, {
                 method: method, // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
+                //mode: 'cors', // no-cors, *cors, same-origin
                 cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'omit', // include, *same-origin, omit
+                //credentials: 'omit', // include, *same-origin, omit
                 headers: headers,
-                redirect: 'manual', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                //redirect: 'manual', // manual, *follow, error
+                //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
                 body: data != null ? data : null // body data type must match "Content-Type" header
             }).then((response) => {
                 //TODO
