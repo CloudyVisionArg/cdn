92bba645cb6461771539f1b2718c928855c833d3
santiagopagnone <spagnone@gmail.com>
2023-04-26 12:39:12 -0300

    .

--- app7/generic.js
+++ app7/generic.js
@@ -1699,6 +1699,7 @@ async function saveDoc(exitOnSuccess) {
         }
 
         try {
+            debugger;
             // Evento afterSave
             $page[0].dispatchEvent(new CustomEvent('afterSave'));
 
@@ -1871,7 +1872,6 @@ function getEvent(pEvent) {
 
 // evalCode con context root
 async function evalCode(code) {
-    debugger;
     var pipe = {};
     eval(`pipe.fn = async () => {\n\n${code}\n};`);
     await pipe.fn();
