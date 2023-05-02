da78633cbd96c87b2f7b9ef2d78470ccd79c7472
Jorge Pagano <jorgepagano@gmail.com>
2023-05-02 18:00:39 -0300

    /

--- include.js
+++ include.js
@@ -354,7 +354,7 @@ function scriptSrc(scriptId, version) {
 /**
 */
 function gitCdn(options) {
-    var url = 'http://10.21.21.223:8081/github?';
+    var url = 'https://10.21.21.223:8081/github?';
     url += getOpt(options, 'owner');
     url += getOpt(options, 'repo');
     url += getOpt(options, 'path');
