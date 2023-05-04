1dddff12835420cf933977157fd174bb0aea3d23
Jorge Pagano <jorgepagano@gmail.com>
2023-05-04 10:52:09 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -976,6 +976,7 @@ async function renderControls(pCont, pParent) {
 
         if (ctl.attr('showtooltip') == '1' && ctl.attr('tooltip')) {
             $this.attr('data-bs-toggle', 'tooltip');
+            $this.attr('data-bs-placement', 'bottom');
             $this.attr('title', ctl.attr('tooltip'));
         }
 
