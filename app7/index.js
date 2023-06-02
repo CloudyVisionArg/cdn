2a39f04e314815c3d445e276388c6ced277e38c9
santiagopagnone <spagnone@gmail.com>
2023-06-02 10:58:44 -0300

    .

--- app7/index.js
+++ app7/index.js
@@ -22,6 +22,7 @@ initScripts.push({ id: 'lib-filesaver' });
 initScripts.push({ id: 'app7-index.css' });
 
 (async () => {
+    debugger;
     // https://docs.sheetjs.com/docs/
     include('xlsx', 'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js');
     
