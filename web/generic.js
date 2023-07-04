6193aa3337d620a3c750be12ccd80e7440af4083
Jorge Pagano <jorgepagano@gmail.com>
2023-07-04 08:52:16 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -293,10 +293,7 @@ async function renderPage() {
         try {
             // Control Event BeforeRender
             var ev = getEvent('BeforeRender');
-            if (ev) {
-                let ret = await evalCode(ev);
-                debugger;
-            };
+            if (ev) await evalCode(ev);
 
         } catch(err) {
             console.error(err);
