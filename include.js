522091d838058c474198467cde0ab39434c8cee0
Jorge Pagano <jorgepagano@gmail.com>
2023-08-16 19:37:29 -0300

    /

--- include.js
+++ include.js
@@ -21,7 +21,7 @@ function registeredScripts() {
     
     // Dps de comitear freshear: https://cdn.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
 
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 300 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 299 });
     scripts.push({ id: 'app7-sync', path: '/app7/sync.js', version: 296 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 296, hasdep: true });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 296 });
