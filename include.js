3642a33ccb364a1c5a63053780e647a100d39c48
Jorge Pagano <jorgepagano@gmail.com>
2023-08-29 16:05:41 -0300

    /

--- include.js
+++ include.js
@@ -22,14 +22,17 @@ function registeredScripts() {
     // Dps de comitear freshear: https://cdn.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
 
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 307 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 308 });
+    scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 308 });
+    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 308 });
+    //scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 291 });
+    //scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 290 });
+    //scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 306 });
     //scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 296 });
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 306 });
     scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 305 });
     scripts.push({ id: 'app7-sync', path: '/app7/sync.js', version: 296 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 296, hasdep: true });
     scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 297 });
-    scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 291 });
-    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 290 });
     scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 283, hasdep: true });
     scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 271 });
     scripts.push({ id: 'app7-scrversions', path: '/app7/scrversions.js', version: 267 });
