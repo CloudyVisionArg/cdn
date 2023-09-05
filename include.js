e57157ea1b4ec80ed5cccff24677e0f9468ef209
Jorge Pagano <jorgepagano@gmail.com>
2023-09-05 12:56:22 -0300

    /

--- include.js
+++ include.js
@@ -25,8 +25,10 @@ function registeredScripts() {
     scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 313 });
     scripts.push({ id: 'app7-notifications', path: '/app7/notifications.js', version: 313 });
     scripts.push({ id: 'app7-popovers.json', path: '/app7/popovers.json', version: 313 });
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 315 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 316 });
+    scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 316, hasdep: true });
     /*
+    scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 311, hasdep: true });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 310 });
     scripts.push({ id: 'app7-popovers.json', path: '/app7/popovers.json', version: 229 });
     scripts.push({ id: 'app7-notifications', path: '/app7/notifications.js', version: 262 });
@@ -34,7 +36,6 @@ function registeredScripts() {
     scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 271 });
     */
 
-    scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 311, hasdep: true });
     scripts.push({ id: 'app7-login', path: '/app7/login.html', version: 310 });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 312 });
     scripts.push({ id: 'app7-cloudy-index.css', path: '/app7/cloudy/index.css', version: 311 });
