d0bcf8269318a3c1ccc674137cd15fa5afc2790e
Jorge Pagano <jorgepagano@gmail.com>
2024-01-16 12:00:47 -0300

    /

--- include.js
+++ include.js
@@ -25,14 +25,15 @@ function registeredScripts() {
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 342 });
     scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 342 });
     scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 343, hasdep: true });
+    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 344 });
     /*
+    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 335 });
     scripts.push({ id: 'web-javascript', path: '/web/javascript.js', version: 325, hasdep: true });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 336 });
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 339 });
     scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 337 });
     */
 
-    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 335 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 334, hasdep: true });
     scripts.push({ id: 'app7-controls', path: '/app7/controls.js', version: 334 });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 333 });
