3bd833976779c71b168204980063841f2d57ecb5
Daniel Paz <danielfernandopaz@hotmail.com>
2024-04-04 10:00:06 -0300

    .

--- web/controls.js
+++ web/controls.js
@@ -999,6 +999,8 @@ async function newAutocomplete(pId, pLabel, options){
                 //option.appendTo($oSel).trigger('change');
                 option.appendTo($oSel);
             }
+
+            $oSel.select2("data",option)
         }
         //$oSel.append(option).trigger('change');
         /*var data = { text : $oSel[0].options[0].text, value : $oSel[0].options[0].value};        
