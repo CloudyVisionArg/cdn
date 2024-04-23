4f58e87c2d70569e465a6af63a21035f6962f671
Marcos Nicolas Chiacchio <marcos@cloudyvision.com.ar>
2024-04-23 12:43:58 -0300

    .

--- web/controls.js
+++ web/controls.js
@@ -1108,7 +1108,7 @@ async function newAutocomplete(pId, pLabel, options){
 
     $oSel.on("select2:clear", function(e){       
         console.log("select2:clear", $oSel.select2("data"));
-        e.preventDefaul();
+        e.preventDefault();
         e.stopPropagation();
         e.stopImmediatePropagation();
         //$oSel[0]._setXmlVal(e.currentTarget);
