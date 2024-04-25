96179d1cdceb5e20ddc0c13c0313bbe492ae3df9
Jorge Pagano <jorgepagano@gmail.com>
2024-04-25 18:23:53 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -16,7 +16,7 @@ var inApp = typeof app7 == 'object';
             { id: 'jquery', src: 'https://code.jquery.com/jquery-3.6.0.min.js' },
             { id: 'bootstrap', src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' },
             { id: 'bootstrap-css', depends: ['bootstrap'], src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' },
-            //{ id: 'web-javascript', depends: ['jquery', 'bootstrap'] },
+            { id: 'web-javascript', depends: ['jquery', 'bootstrap'] },
         ]);
         preloader.show();
         await include([
