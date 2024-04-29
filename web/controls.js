3e1768d8b14acde2ba8383061b4c7b1a5e506b9c
Daniel Paz <danielfernandopaz@hotmail.com>
2024-04-29 11:07:01 -0300

    .

--- web/controls.js
+++ web/controls.js
@@ -1169,27 +1169,22 @@ async function newAutocomplete(pId, pLabel, options){
         if(!option.id || !pOptions.editUrl || $oSel[0].getAttribute("data-valuefield") == "[NULL]")
             return option.text;
 
-        let editUrl = pOptions.editUrl + "&callBackFunction=reloadAutocomplete&doc_id=" + option.id
+        let editUrl = pOptions.editUrl + "&doc_id=" + option.id
         //let $itemObj = $(`<a title="Editar elemento" href="${editUrl}" target="_blank" id="${pId}"> 
-        let $itemObj = $(`<a title="Editar elemento" id="${pId}">        
-            <b class="text-primary">${option.text}</b>              
+        let $itemObj = $(`<a title="Editar elemento" id="${pId}">
+            <b class="text-primary">${option.text}</b>
         </a>`);
 
         // Definir la función de callback cuando se cierre la ventana
         $itemObj.on('click', function() {
             let nuevaVentana = window.open(editUrl, '_blank', 'width=600,height=400');
             $(nuevaVentana).on('beforeunload', function() {
-                reloadAutocomplete();
+                debugger;
             });
         });
         
         return $itemObj;
     }
-}
-
-function reloadAutocomplete(ev){
-    var aux = ev;
-    debugger;
 }
 
 //Funciones agregadas que tal vez no deban estar aca sino en algun lugar mas global
