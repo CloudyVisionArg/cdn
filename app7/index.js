8c091268ca47e9c192dde7e164502b1cc29aced8
Jorge Pagano <jorgepagano@gmail.com>
2024-05-02 10:25:50 -0300

    /

--- app7/index.js
+++ app7/index.js
@@ -167,6 +167,9 @@ var app = {
                         var context = getRouterContext(arguments);
                         let params = context.to.params;
                         let url = 'https://cdn.cloudycrm.net/gh/' + params.owner + '/' + params.repo + '/' + params.path;
+                        // Esto es para poder enviar file.js!_fresh=1 que se tome como file.js?_fresh=1
+                        // Si lo pasas con ? se rompe
+                        url = url.replaceAll('!', '?');
                         loadJS(url, context.to, context.from, context.resolve, context.reject);
                     }
                 },
@@ -176,6 +179,7 @@ var app = {
                         var context = getRouterContext(arguments);
                         let params = context.to.params;
                         let url = 'https://cdn.cloudycrm.net/ghcv/' + params.repo + '/' + params.path;
+                        url = url.replaceAll('!', '?');
                         debugger;
                         loadJS(url, context.to, context.from, context.resolve, context.reject);
                     }
