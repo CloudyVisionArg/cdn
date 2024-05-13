ef1bcfd2856bf750f4b1e6feda2ff7b0ecf177bf
Jorge Pagano <jorgepagano@gmail.com>
2024-05-13 16:00:18 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -71,7 +71,7 @@ var propControls = 'App7_controls';
             folder = await dSession.folder(fld_id);
             folder.form; // Para q vaya cargando el form
 
-            document.addEventListener(afterRender, async (ev) => {
+            document.addEventListener('afterRender', async (ev) => {
                 await folder.search();
                 console.log('ev mgr');
             });
