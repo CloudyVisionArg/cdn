86074c7e38af0bfa1bd940084c20276ccb718b9b
Jorge Pagano <jorgepagano@gmail.com>
2024-05-21 12:18:46 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -625,7 +625,6 @@ async function webRenderPage() {
         ctl = modControls.newDocLog('docLog', {
             label: 'Cambios de datos',
             collapse: false,
-            compact: true,
         });
         ctl.control.addClass('mt-3').appendTo($tab);
 
