463cc9211712b425a26512cff55c176b75d04634
Jorge Pagano <jorgepagano@gmail.com>
2024-05-31 18:15:39 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1158,7 +1158,7 @@ async function fillControls() {
         this.drs.value(doc);
     });
 
-    $get('[data-role="doc-log"]').each(function (ix, el) {
+    $get('[data-role*="doc-log"]').each(function (ix, el) {
         this.drs.value(doc);
     });
 
