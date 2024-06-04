60593a946a25a73e2ea4a3692fe77444848b0987
Jorge Pagano <jorgepagano@gmail.com>
2024-06-04 18:13:03 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1098,9 +1098,10 @@ async function renderControls(container, parent) {
 
             if (ctl['SCRIPTBEFORERENDER']) {
                 debugger;
-                let pipe = {}
-                eval('pipe.fn = ' + evalCode.toString());
-                await pipe.fn(ctl['SCRIPTBEFORERENDER'], context);
+                //let pipe = {}
+                //eval('pipe.fn = ' + evalCode.toString());
+                let pipe = eval(evalCode.toString());
+                await pipe(ctl['SCRIPTBEFORERENDER'], context);
             }
 
         } catch (err) {
