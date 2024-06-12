e0bacbda4bb5d15b3b618befbefedfa0f8093623
Jorge Pagano <jorgepagano@gmail.com>
2024-06-12 11:54:55 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1050,6 +1050,7 @@ async function renderControls(container, parent) {
 
         } else if (type == 'FIELDSET') {
 
+            debugger;
             options = ctl['DESCRIPTION'] ? { label: ctl['DESCRIPTION'] } : { noBorders: true };
 
             if (ctl['W'] == 0 || ctl.attr('readonly') == '1') {
