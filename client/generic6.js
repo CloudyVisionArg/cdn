3de4be0e82b9e540765d7e7de2652d8c7f8f1715
Jorge Pagano <jorgepagano@gmail.com>
2024-06-24 17:58:36 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1058,7 +1058,7 @@ async function renderControls(container, parent) {
             $this = control.$root;
 
             if (!inApp) {
-                control.$root.addClass('mt-3');
+                control.$root.addClass('mt-5');
             }
 
 
