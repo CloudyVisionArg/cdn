f5f524bd74dc944d1bd5bb479e8fc29a35088d38
santiagopagnone <spagnone@gmail.com>
2024-07-01 18:22:38 -0300

    .

--- web/generic.js
+++ web/generic.js
@@ -51,9 +51,9 @@ arrScriptsPos.push({ id: 'font-awesome', src: 'https://netdna.bootstrapcdn.com/f
 arrScriptsPos.push({ id: 'ckeditor', src: '/c/inc/ckeditor-nov2016/ckeditor.js' });
 arrScriptsPos.push({ id: 'lib-filesaver' });
 debugger;
+arrScriptsPos.push({id: 'monaco-editor-css', src: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs/editor/editor.main.min.css' });
 arrScriptsPos.push({id: 'monaco-editor', src: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs/editor/editor.main.min.js' });
 arrScriptsPos.push({id: 'monaco-editor-loader', src: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs/loader.js' });
-arrScriptsPos.push({id: 'monaco-editor-css', src: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs/editor/editor.main.min.css' });
 
 
 
