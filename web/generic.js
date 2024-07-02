14cf5b28e0d277dd78bee977862985aea0122121
santiagopagnone <spagnone@gmail.com>
2024-07-02 19:06:42 -0300

    .

--- web/generic.js
+++ web/generic.js
@@ -89,10 +89,10 @@ debugger;
 
     Doors.RESTFULL.ServerUrl = dSession.serverUrl;
     Doors.RESTFULL.AuthToken = dSession.authToken;
-
+    var require = { paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs' } };
     include(arrScriptsPos).then(()=>{
         document.getElementById("script_monaco-editor-main-css").setAttribute("data-name","vs/editor/editor.main");
-        var require = { paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs' } };
+        //var require = { paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs' } };
         var monacoEditorContainer = document.createElement("div");
         monacoEditorContainer.id = "monaco-editor-cont";
         monacoEditorContainer.height = "400px";
