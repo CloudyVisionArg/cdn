9d4fb42e8e23e7f284ab649d16e5b2cb775ae981
santiagopagnone <spagnone@gmail.com>
2024-07-02 19:28:37 -0300

    .

--- web/generic.js
+++ web/generic.js
@@ -57,6 +57,9 @@ arrScriptsPos.push({ id: 'lib-filesaver' });
 arrScriptsPos.push({id: 'monaco-editor-loader',  src: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs/loader.js' });
 arrScriptsPos.push({id: 'monaco-editor-main-nls', src: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs/editor/editor.main.nls.js' });
 arrScriptsPos.push({id: 'monaco-editor-main-js', src: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs/editor/editor.main.js' });
+arrScriptsPos.push({id: 'monaco-editor-main-javascript-js', src: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs/basic-languages/javascript/javascript.js' });
+arrScriptsPos.push({id: 'monaco-editor-main-ts-js', src: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.49.0/min/vs/language/typescript/tsMode.js' });
+
 
 
 //arrScriptsPos.push({id: 'monaco-editor-loader', src: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs/loader.js',  depends:['jquery','monaco-editor']});
@@ -105,14 +108,14 @@ debugger;
         monacoEditorContainer.id = "monaco-editor-cont";
         document.querySelector("body").append(monacoEditorContainer);
         document.querySelector("#monaco-editor-cont").style.height = "400px";
-        // var editor = monaco.editor.create(monacoEditorContainer, {
-        //     value: [
-        //         'function x() {',
-        //         '\tconsole.log("Hello world!");',
-        //         '}'
-        //     ].join('\n'),
-        //     language: 'javascript'
-        // });
+        var editor = monaco.editor.create(monacoEditorContainer, {
+            value: [
+                'function x() {',
+                '\tconsole.log("Hello world!");',
+                '}'
+            ].join('\n'),
+            language: 'javascript'
+        });
     });
 
     await dSession.runSyncEventsOnClient(false);
