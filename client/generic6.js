327a6decf58daaefbc460620d4c1d663dc860219
Jorge Pagano <jorgepagano@gmail.com>
2024-07-18 10:23:03 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -132,7 +132,6 @@ async function loadControls() {
         // controls hija
         try {
             if (!controlsFolder) {
-                //controlsFolder = (await folder.folders()).get('controls');
                 controlsFolder = await folder.folders('controls');
             }
         } catch(er) {};
