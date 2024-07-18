c5773b2d216c3a211e9a06c71c0e821f68cfc129
Jorge Pagano <jorgepagano@gmail.com>
2024-07-18 16:46:29 -0300

    /

--- web/generic.js
+++ web/generic.js
@@ -1375,6 +1375,7 @@ async function saveDoc(exitOnSuccess) {
         $('[data-textfield]').each(function (ix, el) {
             var $el = $(el);
             var field = doc.fields($el.attr('data-textfield'));
+            if (field == 'fecha') debugger;
 
             if (field && field.updatable) {
                 if (el._text) {
