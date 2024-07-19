8c662d2d43543e82bfe6f7be13f7903e0b8bd77f
Jorge Pagano <jorgepagano@gmail.com>
2024-07-19 19:27:39 -0300

    /

--- include.js
+++ include.js
@@ -23,7 +23,7 @@ function registeredScripts() {
     scripts.push({ id: 'maps', path: '/maps.js', version: 370 });
     scripts.push({ id: 'app7-explorer', path: '/app7/explorer.js', version: 370 });
     scripts.push({ id: 'app7-global', path: '/app7/global.js', version: 370, hasdep: true });
-    scripts.push({ id: 'generic6', path: '/client/generic6.js', version: 370 });
+    scripts.push({ id: 'generic6', path: '/client/generic6.js', version: 371 });
     scripts.push({ id: 'web-controls', path: '/web/controls.js', version: 369 });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 368 });
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 368 });
