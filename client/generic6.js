2b231039df08ffd08926ca2b00dc364b5606f30d
Jorge Pagano <jorgepagano@gmail.com>
2024-07-19 23:02:22 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1691,7 +1691,7 @@ function showDesigner() {
             let $el = $(el);
             let $lnk = $('<a />', {
                 class: 'doors-container-label',
-                style: 'position: fixed; top: -10px; left: -10px;'
+                style: 'position: relative; top: -10px; left: 10px;'
             }).append($el.attr('data-drs-id'));
             $el.prepend($lnk);
         })
