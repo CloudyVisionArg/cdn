77c6212a43ad40b69015f16e059460150d71d291
Jorge Pagano <jorgepagano@gmail.com>
2024-07-20 00:47:30 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -733,21 +733,19 @@ async function webRenderPage() {
     
     // Modal
     let $modal = $(`
-        <div class="modal fade" tabindex="-1">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title">Data</h5>
-                    <button type="button" class="close" data-bs-dismiss="modal">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                    ...
-                    </div>
+    <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
                 </div>
             </div>
         </div>
+    </div>
     `);
 
     modal = new bootstrap.Modal($modal);
