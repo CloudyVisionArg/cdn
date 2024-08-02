845e613f8c25c7fa1d7a2199287d015ef7d25c2e
Jorge Pagano <jorgepagano@gmail.com>
2024-08-02 10:47:36 -0300

    /

--- client/generic6.js
+++ client/generic6.js
@@ -1655,7 +1655,7 @@ async function saveDoc(exitOnSuccess) {
         } else {
             toast('Cambios guardados');
             if (exitOnSuccess) {
-                setTimeout(exitForm, inApp ? 0 : 1000);
+                setTimeout(exitForm, inApp ? 0 : 750);
             } else {
                 fillControls();
             }
