63041d35988c5f95b5e3dda4c882ef5f7c3961ef
Jorge Pagano <jorgepagano@gmail.com>
2024-08-16 10:16:31 -0300

    /

--- wapp/wapp.js
+++ wapp/wapp.js
@@ -641,6 +641,8 @@ var wapp = {
 					console.log(err);
 				};
 				if (media) {
+					if (pMsg.sid == 'MM7fe90ba83246627aa31ea7d7872608ae') debugger;
+					
 					media.forEach(it => {
 						// https://www.twilio.com/docs/whatsapp/guidance-whatsapp-media-messages#supported-mime-types
 						
