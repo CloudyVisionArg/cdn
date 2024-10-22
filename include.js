080e842afc2b23fc96be45410d304bf7c177d6b0
Jorge Pagano <jorgepagano@gmail.com>
2024-10-22 11:50:03 -0300

    .

--- include.js
+++ include.js
@@ -23,14 +23,14 @@ function registeredScripts() {
         https://cdn.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
         https://nodedev.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
     */
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 400 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 401 });
     scripts.push({ id: 'generic6', path: '/client/generic6.js', version: 400 });
     scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 381 });
     scripts.push({ id: 'whatsapp-css', path: '/wapp/wapp.css', version: 380 });
     scripts.push({ id: 'lib-moment', path: '/lib/moment-with-locales.min.js', version: 377 });
     /*
     Por si hay que volver atras
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 399 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 400 });
     scripts.push({ id: 'generic6', path: '/client/generic6.js', version: 393 });
     scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 352 });
     scripts.push({ id: 'whatsapp-css', path: '/wapp/wapp.css', version: 56 });
