fe007b327a6432a1a1d773a62f9994cdd41ac9e0
Jorge Pagano <jorgepagano@gmail.com>
2024-12-03 12:21:24 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -5025,36 +5025,8 @@ export class Utilities {
             }
         }
         return me.deepAssign(target, ...sources);
+    }
 
-        /*
-        function isObject(item) {
-            return (item && typeof item === 'object' && !Array.isArray(item));
-        }
-        */
-    }
-    /*
-export function isObject(item) {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-  }
-  
-  export function mergeDeep(target, ...sources) {
-    if (!sources.length) return target;
-    const source = sources.shift();
-  
-    if (isObject(target) && isObject(source)) {
-      for (const key in source) {
-        if (isObject(source[key])) {
-          if (!target[key]) Object.assign(target, { [key]: {} });
-          mergeDeep(target[key], source[key]);
-        } else {
-          Object.assign(target, { [key]: source[key] });
-        }
-      }
-    }
-  
-    return mergeDeep(target, ...sources);
-  }
-    */
     deserializeError(err) {
         return _serializeError.deserializeError(err);
     }
