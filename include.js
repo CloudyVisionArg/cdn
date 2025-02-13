c616132214ebd73cb6897116aaa76691b1b0d052
Santiago Pagnone <spagnone@gmail.com>
2025-02-13 14:30:30 -0300

    Actualizar la versión del script 'app7-generic' a 424 en include.js

--- include.js
+++ include.js
@@ -26,14 +26,13 @@ function registeredScripts() {
     scripts.push({ id: 'pivotable', repo: 'Global', path: 'client/pivotable.js' });
     scripts.push({ id: 'generic6', repo: 'Global', path: '/client/generic6.js' });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 414 });
-    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 405 });
+    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 424 });
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 405 });
     scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 381 });
     scripts.push({ id: 'whatsapp-css', path: '/wapp/wapp.css', version: 380 });
     /*
     Por si hay que volver atras
-    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 344 });
-    scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 368 });
+    scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 405 });
     scripts.push({ id: 'generic6', path: '/client/generic6.js', version: 404 });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 412 });
     scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 352 });
