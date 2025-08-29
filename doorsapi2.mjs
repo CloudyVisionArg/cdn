b9f10f174ebf9a1d693b753a6391d3d35330f72c
Jorge Pagano <jorgepagano@gmail.com>
2025-08-29 11:59:34 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -440,7 +440,6 @@ export class Session {
             let fldId = docJson.HeadFields.find(el => el['Name'] == 'FLD_ID').Value
             let url = 'folders/' + fldId + '/relfields';
             let res = await me.v8Client.fetch(url);
-        debugger
             res.forEach(el => {
                 el['Value'] = null;
                 el['ValueOld'] = null;
