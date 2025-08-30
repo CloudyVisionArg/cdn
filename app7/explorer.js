64766e67a1a0a19cb02ae7d1cf7c8c815b14115a
Jorge Pagano <jorgepagano@gmail.com>
2025-08-30 12:05:44 -0300

    /

--- app7/explorer.js
+++ app7/explorer.js
@@ -200,8 +200,10 @@ actionsPopup = getActionsPopup();
         if (device.platform == 'browser' || device.platform == 'Android') {
             // El taphold no anda en el browser
             // En Android tampoco funciona el taphold
-            $viewDiv.on('contextmenu', 'a', taphold);
-            debugger;
+            $viewDiv.on('contextmenu', 'a', () => {
+                taphold();
+                debugger;
+            });
         } else {                 
             $viewDiv.on('taphold', 'a', taphold);
         };
