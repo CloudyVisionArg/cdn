2672c7ddb90663956eb4875619264b0f77e40d39
Jorge Pagano <jorgepagano@gmail.com>
2025-09-04 20:39:34 -0300

    /

--- doorsapi2.mjs
+++ doorsapi2.mjs
@@ -5167,8 +5167,9 @@ export class User extends Account {
             try {
                 let url = `accounts/${ me.id }/picture`;
                 let res = await me.session.v8Client.fetch(url, { raw: true });
+                let buf = new SimpleBuffer(await res.arrayBuffer());
                 debugger
-                resolve(res);
+                resolve(buf);
             } catch(err) {
                 reject(err);
             }
