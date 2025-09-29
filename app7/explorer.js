77e5e4b599c3a923ca59baae84c5c4d070b111d7
Jorge Pagano <jorgepagano@gmail.com>
2025-09-29 08:52:23 -0300

    /

--- app7/explorer.js
+++ app7/explorer.js
@@ -774,7 +774,7 @@ async function loadViewSection(pContainer, pCallback) {
 
                     $li = $('<li/>', {
                         class: 'accordion-item',
-                        style: `background-color: var(${ document._f7version >= 8 ? '--f7-toolbar-bg-color,var(--f7-bars-bg-color)' : '--f7-list-item-divider-bg-color' });`,
+                        style: `background-color: var(${ document._f7version >= 8 ? ' --f7-list-group-title-bg-color' : '--f7-list-item-divider-bg-color' });`,
                         value: (value == null ? '__NULL__' : value),
                     }).appendTo($ul);
 
