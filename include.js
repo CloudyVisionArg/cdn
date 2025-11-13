dd400f0864813b14f4fbccaf72f6a691c6fcf5b4
Jorge Pagano <jorgepagano@gmail.com>
2025-11-13 17:41:31 -0300

    /

--- include.js
+++ include.js
@@ -24,7 +24,7 @@ function registeredScripts() {
         https://cdn.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
         https://nodedev.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
     */
-    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 524 });
+    scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 525 });
     scripts.push({ id: 'maps', path: '/maps.js', version: 523 });
     scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 521 });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 520 });
