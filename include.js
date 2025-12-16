4a584133c74fddd58626537b5c7e9996746c2a70
Jorge Pagano <jorgepagano@gmail.com>
2025-12-16 19:29:56 -0300

    /

--- include.js
+++ include.js
@@ -24,16 +24,16 @@ function registeredScripts() {
         https://cdn.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
         https://nodedev.cloudycrm.net/ghcv/cdn/include.js?_fresh=1
     */
+    scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 530 });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 529 });
-    scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 528 });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 526 });
     scripts.push({ id: 'maps', path: '/maps.js', version: 523 });
     scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 521 });
     scripts.push({ id: 'web-generic', path: '/web/generic.js', version: 520 });
     /*
     Por si hay que volver atras
+    scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 528 });
     scripts.push({ id: 'app7-index', path: '/app7/index.js', version: 520 });
-    scripts.push({ id: 'whatsapp', path: '/wapp/wapp.js', version: 518 });
     scripts.push({ id: 'doorsapi2', path: '/doorsapi2.mjs', version: 522 });
     scripts.push({ id: 'maps', path: '/maps.js', version: 370 });
     scripts.push({ id: 'app7-generic', path: '/app7/generic.js', version: 427 });
